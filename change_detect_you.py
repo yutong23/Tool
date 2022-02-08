ba10a4badf66087bc940cf35368a6c8e1028839e
yutong23 <78244145+yutong23@users.noreply.github.com>
2022-02-08 11:32:14 -0600

    Update change_detect_you.py

--- change_detect_you.py
+++ change_detect_you.py
@@ -2,38 +2,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import ruptures as rpt
-import numpy as np
-
-# #qualified participants
-# participant_ids=[i for i in range(1,18)]
-# participant_ids.remove(4)
-# participant_ids.remove(15)
-
-# #collective data processing
-# #since participant `4` dont have data and `15` quit the study, here we need to remove them
-# #Combine all previous data into a big dataframe
-# def pre_collective(participant_ids,keyword):
-#     co_data=pd.DataFrame()
-#     for id_ in participant_ids:
-#         temp_data=pd.read_csv("collective_data/P"+str(id_)+".csv",usecols=["frames_ids","valence","arousal"],nrows=5000)
-#         co_data=pd.concat([co_data, temp_data], axis=1)
-#     up_line=np.mean(co_data[keyword], axis=1)+np.std(co_data[keyword], axis=1)
-#     down_line=np.mean(co_data[keyword], axis=1)-np.std(co_data[keyword], axis=1)
-#     df_1=pd.DataFrame(up_line,columns=[keyword+"_upline"])
-#     df_2=pd.DataFrame(down_line,columns=[keyword+"_downline"])
-#     df_up_down=pd.concat([df_1, df_2], axis=1)
-#     return(df_up_down)
-
-# pre_collective(participant_ids,"valence")
-
-#individual visualization
-import matplotlib.pyplot as plt
 import csv
-import numpy as np
 import scipy
 import numpy as np
 import ruptures as rpt
-import colorsys
 from colorsys import hls_to_rgb
 import pathlib
 from PIL import Image
@@ -41,22 +13,7 @@ from PIL import Image
 
 
 def plot_change_points(ts,ts_change_loc, color, intensity, penalty, participant_id,keyword,ax):
-
-
-#     fig, ax = plt.subplots(figsize = (50,10))
-#     ax.set_title("Participant_" +str(participant_id)+"_Penalty_"+str(penalty)+"_"+keyword,fontsize=40,color='black')
-# #     print(color.shape())
-# #     print(intensity.shape())
-#     plt.scatter(np.arange(ts.shape[0]),intensity, c=color, s=150)
-#     #plt.plot(intensity)
-#     plt.xticks([])
-#     plt.yticks([])
-#     #plt.show()
-#     plt.xlim([0,ts.shape[0]])
-#     plt.draw()
-
-    # use the list index can help us find the block value
-    # print(ts_change_loc)
+    
     positive_negative_id = []
     negative_positive_id = []
     my_label1="postive to negative"
@@ -165,24 +122,7 @@ def individual(participant_ids,id_,penalty,keyword, ax, csv_file_path):
                 val.append(float(row[10]))
                 aro.append(float(row[11]))
                 line_count += 1
-    #     print(f'Processed {line_count} lines.')
-
-    #plt.figure(figsize=(50,10))
-    #plt.plot(val)
-
-    #plt.figure(figsize=(50,10))
-    #plt.plot(aro)
-
-
-    # windows=['flat', 'hanning', 'hamming', 'bartlett', 'blackman']
-
-    #plt.figure()
-    #plt.hist(val, bins=100)
-
-    #plt.figure()
-    #plt.hist(aro, bins=100)
-
-    # print(np.mean(val), np.mean(aro))
+   
     val = val - np.mean(val)   # to make the points much closer
     aro = aro - np.mean(aro)
     # sval = smooth(val, 12, windows[0])  # to ensure the input and output have the same numbe of points
@@ -210,4 +150,3 @@ def individual(participant_ids,id_,penalty,keyword, ax, csv_file_path):
 
 def ind(penalty, id_, participant_ids, ax, path):
     individual(participant_ids,id_,penalty,"Valence", ax, path)
-# pre_collective(participant_ids,"valence")
